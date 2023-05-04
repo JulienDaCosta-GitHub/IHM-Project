df9027a65a265870d111d5c41c08b7262b8afccc
Julien Da Costa <juliendacosta27@gmail.com>
2023-05-04 14:00:46 +0200

    Test rotation

--- main.js
+++ main.js
@@ -58,6 +58,7 @@ loader.load(
 
         // Incliner légèrement le mesh vers l'arrière
         object.rotation.x = -0.99;
+        object.rotation.y = 0.2;
 
         // Ajoute l'objet à la scène
         scene.add(object);
