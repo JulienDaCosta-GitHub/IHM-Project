9f9d2327230f350497bc21413d9340aee6bbd560
Julien Da Costa <juliendacosta27@gmail.com>
2023-05-04 09:33:48 +0200

    Update test cube

--- main.js
+++ main.js
@@ -71,27 +71,41 @@
 
 import * as THREE from 'three';
 
+let alpha, beta, gamma = 0;
+
+window.addEventListener('deviceorientation', (event) => {
+    alpha = event.alpha;
+    beta = event.beta;
+    gamma = event.gamma;
+});
+
+const degToRad = (deg) =>  deg * (Math.PI / 180);
+
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+renderer.setAnimationLoop(animate);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+    color: 0x00ff00
+});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
 
 camera.position.z = 5;
 
 function animate() {
-    requestAnimationFrame( animate );
 
-    // cube.rotation.x += 0.01;
-    // cube.rotation.y += 0.01;
+    cube.rotation.x = degToRad(beta);
+    cube.rotation.y = degToRad(gamma);
+    cube.rotation.z = degToRad(alpha);
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
 }
 
 animate();
